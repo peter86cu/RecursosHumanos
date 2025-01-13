8d8565559306a5c9b8c54d7a11f65cd6dc2a33dd
peter86cu <39494828+peter86cu@users.noreply.github.com>
2025-01-13 01:16:44 -0300

    update v1

--- src/main/java/com/ayalait/rh/repositorio/HorasExtraJpa.java
+++ src/main/java/com/ayalait/rh/repositorio/HorasExtraJpa.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ayalait.rh.modelo.EmpleadoHorasExtra;
 
-public interface HorasExtraJpa extends JpaRepository<EmpleadoHorasExtra, Integer>{
+public interface HorasExtraJpa extends JpaRepository<EmpleadoHorasExtra, String>{
 	
 	
 	
